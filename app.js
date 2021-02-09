fc0817eb7b853762f47d73416284f36f75319196
Amory <garbage90@yandex.ru>
2021-02-09 20:09:53 +0300

    feat: mongoose connect

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const {PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/moviedb',
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
 app.listen(PORT, () => {
   console.log(`Start server on port ${PORT}`);
-})
+});
