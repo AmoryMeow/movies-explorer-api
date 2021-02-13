abdd386e900ded71c259171755b7eca5588311e2
Amory <garbage90@yandex.ru>
2021-02-13 11:02:49 +0300

    feat: check authorization

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = require('express').Router();
 const { createUser, loginUser } = require('../controllers/users');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const { auth } = require('../middleware/auth');
 
 router.post('/signup', createUser);
 router.post('/signin', loginUser);
 
-router.use('/users', usersRouter);
-router.use('/movies', moviesRouter);
+router.use('/users', auth, usersRouter);
+router.use('/movies', auth, moviesRouter);
 
 module.exports = router;
