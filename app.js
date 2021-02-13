78b8aeba0d1256ecddb3a506d6914faa3b5519a5
Amory <garbage90@yandex.ru>
2021-02-13 11:15:26 +0300

    feat: logger _  request, error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const { requestLogger, errorLogger } = require('./middleware/logger');
 const routes = require('./routes/index');
 const errorHandler = require('./middleware/errorHandler');
 
@@ -20,8 +21,11 @@ mongoose.connect(MONGO_URL,
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
+app.use(requestLogger);
+
 app.use(routes);
 
+app.use(errorLogger);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
